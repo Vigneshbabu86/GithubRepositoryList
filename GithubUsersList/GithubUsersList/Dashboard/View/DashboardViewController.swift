b02c2ba96709c1e0e84a59853781d8c6c68b8f86
Vignesh Babu <vignesh.babu@pearson.com>
2019-09-05 23:23:08 -0600

    - Setting up the Initial View Controller

---
+++ GithubUsersList/GithubUsersList/Dashboard/View/DashboardViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardController.swift
+//  GithubUsersList
+//
+//  Created by Vignesh Babu on 9/5/19.
+//  Copyright © 2019 Vignesh Babu. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
