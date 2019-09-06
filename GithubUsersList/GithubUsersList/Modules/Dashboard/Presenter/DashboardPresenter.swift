18bdbfe626230d4876a449d64926547cc0b5e180
Vignesh Babu <vignesh.babu@pearson.com>
2019-09-06 09:14:48 -0600

    - Completed the User Interface Setup

---
+++ GithubUsersList/GithubUsersList/Modules/Dashboard/Presenter/DashboardPresenter.swift
@@ -0,0 +1,13 @@
+//
+//  DashboardPresenter.swift
+//  GithubUsersList
+//
+//  Created by Vignesh Babu on 9/5/19.
+//  Copyright © 2019 Vignesh Babu. All rights reserved.
+//
+
+import Foundation
+
+protocol DashboardPresenter {
+    func getUserProjects()
+}
